b41844eff25acd9fa95a2c37cd8847845042a258
Utkarsh Prakash <114054236+Utkarsh434@users.noreply.github.com>
2025-08-14 04:56:49 +0530

    urll

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ connectDB();
 //middleware
 // CORS Configuration
 const corsOptions = {
-    origin: 'http://localhost:5173', // Your frontend URL
+    origin: 'https://food-share-frontend-taupe.vercel.app/', // Your frontend URL
     optionsSuccessStatus: 200
 };
 app.use(cors(corsOptions));
